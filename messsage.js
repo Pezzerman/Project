9c2ac038e65d54e6bd8bbd4703eea7551f9539d9
Pezzerman <18PerrettL@smrt.bristol.sch.uk>
2025-01-13 09:24:27 +0000

    Update messsage.js

--- messsage.js
+++ messsage.js
@@ -1,3 +1,14 @@
-function msg(){  
- alert("Hello Javatpoint");  
-}  
+$('#test').click(function() {
+    var docHeight = $(document).height(),
+        docWidth = $(document).width(),
+        $div = $('#test'),
+        divWidth = $div.width(),
+        divHeight = $div.height(),
+        heightMax = docHeight - divHeight,
+        widthMax = docWidth - divWidth;
+
+    $div.css({
+        left: Math.floor( Math.random() * widthMax ),
+        top: Math.floor( Math.random() * heightMax )
+    });
+});
