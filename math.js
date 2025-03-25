09e826a4ac6d5b06b2f4444ba07463426d0ae340
Pezzerman <18PerrettL@smrt.bristol.sch.uk>
2025-03-25 20:41:31 +0000

    Update and rename bigTrinket.js to math.js

---
+++ math.js
@@ -0,0 +1,9 @@
+// Trinkets collected when clicking the big trinket
+function trinketCollection() {
+  // Gets the current amount of trinkets (starts at zero)
+  const trinketHTML = document.getElementById("trinkets");
+  // The 10 here makes sure its in base 10. Just adds one per click for now, can easily be expanded upon later
+  const trinket = parseInt(trinketHTML.innerText, 10) + 1;
+  // Updates HTML element for total trinkets
+  document.getElementById("trinkets").innerText = trinket;
+} 
