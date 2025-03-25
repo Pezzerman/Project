3134094e0f77603aa1b0f565226e4ef5063dcbf3
Pezzerman <18PerrettL@smrt.bristol.sch.uk>
2025-03-25 19:52:15 +0000

    Update bigTrinket.js

--- bigTrinket.js
+++ bigTrinket.js
@@ -1,5 +1,5 @@
 // This sets the trinket value to 0
 // let trinket = 0;
 // This sets the 
-document.getElementById('trinkets') = 10;
+document.getElementById('trinkets').innerText = 10;
 // const bigTrinket = document.getElementById('trinket-button');
