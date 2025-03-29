dde0d24dcb6faccaeb451ee1955bd25088fb43a9
Pezzerman <18PerrettL@smrt.bristol.sch.uk>
2025-03-29 12:52:33 +0000

    Update math.js

--- math.js
+++ math.js
@@ -1,16 +1,18 @@
 
 // Trinkets collected when clicking the big trinket
 function trinketCollection() {
-  // Gets the current amount of trinkets (starts at zero)
   const trinketHTML = document.getElementById("trinkets");
-  // The 10 here makes sure its in base 10. Just adds one per click for now, can easily be expanded upon later
+  // The 10 here makes sure its in base 10. Adds one to the value in trinkets id
   const trinket = parseInt(trinketHTML.innerText, 10) + 1;
-  // Updates HTML element for total trinkets
   document.getElementById("trinkets").innerText = trinket;
 } 
 
+// adds one to the current amount of uncollect trinkets
 function generationSpeed() {
-  let number = Math.floor(Math.random() * 10);
-  setTimeout(repeat, 1000);
-  document.getElementById("testing").innerText = number;
+  const uncollectedTrinkets = document.getElementById("uncollectedTrinkets")
+  const newUncollectedTrinkets = parseInt(uncollectedTrinkets.innerText, 10) + 1;
+  document.getElementById("uncollectedTrinkets").innerText = newUncollectedTrinkets;
 }
+
+// Trinket generation speed interval. 1000 = 1000 milliseconds = 1 second
+setInterval(generationSpeed, 1000);
