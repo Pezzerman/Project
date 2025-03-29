7b46fe70ef7a5eab3bfe7a9378945a36076a7a48
Pezzerman <18PerrettL@smrt.bristol.sch.uk>
2025-03-29 14:02:46 +0000

    Update math.js

--- math.js
+++ math.js
@@ -1,17 +1,26 @@
 
 // Trinkets collected when clicking the big trinket
 function trinketCollection() {
+  const uncollectedTrinkets = document.getElementById("uncollectedTrinkets");
+  const currentTrinkets = parseInt(uncollectedTrinkets.textContent, 10);
   const trinketHTML = document.getElementById("trinkets");
   // The 10 here makes sure its in base 10. Adds one to the value in trinkets id
-  const trinket = parseInt(trinketHTML.innerText, 10) + 1;
-  document.getElementById("trinkets").innerText = trinket;
+  const trinket = parseInt(trinketHTML.textContent, 10) + currentTrinkets;
+  document.getElementById("trinkets").textContent = trinket;
+  document.getElementById("uncollectedTrinkets").textContent = 0;
 } 
 
 // adds one to the current amount of uncollect trinkets
 function generationSpeed() {
-  const uncollectedTrinkets = document.getElementById("uncollectedTrinkets")
-  const newUncollectedTrinkets = parseInt(uncollectedTrinkets.innerText, 10) + 1;
-  document.getElementById("uncollectedTrinkets").innerText = newUncollectedTrinkets;
+  // gets the current amount in the element and parses (converts it) into an integer
+  const uncollectedTrinkets = document.getElementById("uncollectedTrinkets");
+  const currentTrinkets = parseInt(uncollectedTrinkets.textContent, 10);
+  
+  // if statement is here to cap it at 10
+  if (currentTrinkets < 10) {
+    const newUncollectedTrinkets = currentTrinkets + 1;
+    uncollectedTrinkets.textContent = newUncollectedTrinkets;
+  }
 }
 
 // Trinket generation speed interval. 1000 = 1000 milliseconds = 1 second
