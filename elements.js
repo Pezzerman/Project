ba8a8232b78d26393d54f7dfd1addb290067c610
Pezzerman <18PerrettL@smrt.bristol.sch.uk>
2025-04-03 22:19:19 +0100

    Update elements.js

--- elements.js
+++ elements.js
@@ -1,8 +1,14 @@
 function trinketUpgradeScreen1() {
   let trinketUpgradeScreen = document.getElementById("trinket-upgrade-1-screen");
+  let button = document.getElementById("trinket-upgrade-1");
+  let title = document.getElementById("trinket-upgrade-1-text");
   if (trinketUpgradeScreen.style.display === "none") {
     trinketUpgradeScreen.style.display = "block";
+    button.style.display = "none";
+    title.style.display = "none";
   } else {
     trinketUpgradeScreen.style.display = "none";
+    button.style.display = "block";
+    title.style.display = "block";
   }
 }
