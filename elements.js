55015fb4191f24566909845714dc577bfdfc3d5e
Pezzerman <18PerrettL@smrt.bristol.sch.uk>
2025-04-03 16:05:38 +0100

    Create elements.js

---
+++ elements.js
@@ -0,0 +1,8 @@
+function trinketUpgradeScreen1() {
+  let trinketUpgradeScreen = document.getElementById("trinket-upgrade-1-screen");
+  if (trinketUpgradeScreen.style.display === "none") {
+    trinketUpgradeScreen.style.display = "block";
+  } else {
+    trinketUpgradeScreen.style.display = "none";
+  }
+}
